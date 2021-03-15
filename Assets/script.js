a6f865a6da44b53023be14bb308e04d0599363c0
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-15 22:03:28 +0000

    added confirms for characters in password

--- Assets/script.js
+++ Assets/script.js
@@ -4,14 +4,19 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var password = "";
   const passwordLength = prompt("What length do you want your password to be?");
-  // console.log(passwordLength);
+  // Validates the length of the password.
   if (passwordLength >= 8 && passwordLength <= 128) {
-    console.log("Correct");
-    return password;
+    console.log("correct"); // Confirms for characters in password.
+    const specialChar = confirm(
+      "Do you want to include any special characters?"
+    );
+    const upperCase = confirm("Do you want to include any UPPER CASE letters?");
+    const lowerCase = confirm("Do you want to include any lower case letters?");
+    const numerical = confirm("Do you want to include any numbers?");
   } else {
     const tryAgain = alert("Choose a length between 8 and 128 characters.");
-    console.log("Try again");
   }
+  // return password;
 }
 
 // Write password to the #password input
