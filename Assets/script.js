0af08b9bd07453b5b093bd5f58f0ae35cb4610a0
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-15 21:20:03 +0000

    created function for generating password length

--- Assets/script.js
+++ Assets/script.js
@@ -4,9 +4,14 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var password = "";
   const passwordLength = prompt("What length do you want your password to be?");
-  console.log(passwordLength);
-
-  return password;
+  // console.log(passwordLength);
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log("Correct");
+    return password;
+  } else {
+    const tryAgain = alert("Choose a length between 8 and 128 characters.");
+    console.log("Try again");
+  }
 }
 
 // Write password to the #password input
