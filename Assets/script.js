c6d7d36a1bd13e23be97b9d5a49f8f407acf2bed
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-16 20:23:37 +0000

    added arrays WIP and moved confirm buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,84 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// const lowercaseArray = [
+//   a,
+//   b,
+//   c,
+//   d,
+//   e,
+//   f,
+//   g,
+//   h,
+//   i,
+//   j,
+//   k,
+//   l,
+//   m,
+//   n,
+//   o,
+//   p,
+//   q,
+//   r,
+//   s,
+//   t,
+//   u,
+//   v,
+//   w,
+//   x,
+//   y,
+//   z,
+// ];
+// const uppercaseArray = [
+//   A,
+//   B,
+//   C,
+//   D,
+//   E,
+//   F,
+//   G,
+//   H,
+//   I,
+//   J,
+//   K,
+//   L,
+//   M,
+//   N,
+//   O,
+//   P,
+//   Q,
+//   R,
+//   S,
+//   T,
+//   U,
+//   V,
+//   W,
+//   X,
+//   Y,
+//   Z,
+// ];
+// const specialCharactersArray = ["[ ]  ^ $ . | ? * + ( )"];
+
 function generatePassword() {
   var password = "";
   const passwordLength = prompt("What length do you want your password to be?");
   // Validates the length of the password.
   if (passwordLength >= 8 && passwordLength <= 128) {
-    console.log("correct"); // Confirms for characters in password.
-    const specialChar = confirm(
-      "Do you want to include any special characters?"
-    );
-    const upperCase = confirm("Do you want to include any UPPER CASE letters?");
-    const lowerCase = confirm("Do you want to include any lower case letters?");
-    const numerical = confirm("Do you want to include any numbers?");
+    console.log("correct"); // Confirms for number of characters in password.
+    const isSpecial = confirm("Do you want to include any special characters?");
   } else {
     const tryAgain = alert("Choose a length between 8 and 128 characters.");
   }
+  if ((isSpecial = confirm)) {
+    const isUpperCase = confirm(
+      "Do you want to include any UPPER CASE letters?"
+    );
+    const isLowerCase = confirm(
+      "Do you want to include any lower case letters?"
+    );
+    const isNumbers = confirm("Do you want any numbers?");
+  }
+  console.log("all correct");
   // return password;
 }
 
