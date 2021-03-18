ca72cbe4dc530d9e5555b96e99675f2833e0941c
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-18 20:18:45 +0000

    fixed confirms

--- Assets/script.js
+++ Assets/script.js
@@ -69,6 +69,8 @@ let isUpperCase;
 let isNumbers;
 let isSpecial;
 
+let password = [];
+
 //pushing getOptionsArray from options array.
 function getOptionsArray() {
   if (isLowerCase) {
@@ -93,7 +95,6 @@ function collectPassword() {
   for (let i = 0; i < passwordLength; i++) {
     const characters = passwordArray.push(characters);
   }
-  return collectPassword;
 }
 
 //choosing random characters
@@ -112,32 +113,38 @@ const generatePassword = function () {
     passwordLength = parseInt(passwordLength, 10);
     console.log(typeof passwordLength);
     passwordLength;
+    // Confirms for character types.
+    if (passwordLength) {
+      isSpecial = confirm("Do you want to include any special characters?");
+      console.log(isSpecial);
 
-    // Confirm for number of characters in password.
+      isUpperCase = confirm(
+        "Do you want to include any upper case characters?"
+      );
+      console.log(isUpperCase);
+
+      isLowerCase = confirm(
+        "Do you want to include any lower case characters?"
+      );
+      console.log(isLowerCase);
+
+      isNumbers = confirm("Do you want to include any numbers?");
+      console.log(isNumbers);
+    }
+
+    getOptionsArray();
+    console.log("hello");
+
+    //alert for character numbers
   } else {
     const tryAgain = alert("Choose a length between 8 and 128 characters.");
   }
 
-  // Confirms for character types.
-  if (passwordLength) {
-    isSpecial = confirm("Do you want to include any special characters?");
-    console.log(isSpecial);
-
-    isUpperCase = confirm("Do you want to include any upper case characters?");
-    console.log(isUpperCase);
-
-    isLowerCase = confirm("Do you want to include any lower case characters?");
-    console.log(isLowerCase);
-
-    isNumbers = confirm("Do you want to include any numbers?");
-    console.log(isNumbers);
-  }
   // getOptionsArray();
   // collectPassword();
   // getCharacters();
 
-  console.log(password);
-  return password;
+  // console.log(password);
 };
 
 // return password;
